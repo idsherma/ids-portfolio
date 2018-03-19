dfb456e676250f7fc851f7216cbe41205678ba99
Ivory Sherman <ivorysherman@Ivorys-MBP.home>
2018-03-18 21:45:01 -0400

    progress started; design better

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
-$(function() {
-  $(".animated-line").typed({
-    strings: ["DESIGNER", "DEVELOPER", "CREATIVE"],
-    typeSpeed: 50,
-    backSpeed: 10,
-    backDelay: 2000,
-    showCursor: false,
-    loop: false
-  });
-});
+(function(scs) {
+    scs(window.jQuery, window, document);
+    }(function($, window, document) {
+        // jQuery $ locally scoped
+       
+        
+        $(function() {
+            
+
+            //overlay menu for mobile
+            function overlayMenu() {
+              $('.menu-icon').on('click', function() {
+                $('.menu-icon').toggleClass('active');
+                $('.overlay-nav').toggleClass('active');
+                $('body').toggleClass("overflow");
+              });
+            }
+
+            overlayMenu();
+
+            //closing hamburger menu overlay
+            $('.nav-content ul li').on('click',function(event){
+                $('.menu-icon').removeClass('active');
+                $('.overlay-nav').removeClass('active');
+                $('body').removeClass("overflow");
+            });
+
+            //adding + removing active class on menu overlay
+            $('ul li a').click(function(){
+                $('li a').removeClass("active");
+                $(this).addClass("active");
+            });
+
+
+            // $(".animated-line").typed({
+            //     strings: ["DESIGNER", "DEVELOPER", "CREATIVE"],
+            //     typeSpeed: 50,
+            //     backSpeed: 10,
+            //     backDelay: 2000,
+            //     showCursor: false,
+            //     loop: false
+            // });
+
+            
+        });
+    })
+);
