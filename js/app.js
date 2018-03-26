077abcba42226405087027fd913ab8a5f071a79a
Ivory Sherman <ivorysherman@Ivorys-MBP.home>
2018-03-25 21:41:56 -0400

    added portfolio and footer section; need color help

--- js/app.js
+++ js/app.js
@@ -61,11 +61,68 @@
                 $(this).addClass("active");
             });
 
-            var wow = new WOW({
-              boxClass: 'js-wow'
-            });
+            // init Masonry
+            var $grid = $('.img-grid').imagesLoaded( function() {
+              $grid.masonry({
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                columnWidth: '.grid-sizer'
+              }); 
+            });  
 
-            wow.init();
+          //   (function($) {
+
+          //   /**
+          //    * Copyright 2012, Digital Fusion
+          //    * Licensed under the MIT license.
+          //    * http://teamdf.com/jquery-plugins/license/
+          //    *
+          //    * @author Sam Sehnert
+          //    * @desc A small plugin that checks whether elements are within
+          //    *     the user visible viewport of a web browser.
+          //    *     only accounts for vertical position, not horizontal.
+          //    */
+
+          //   $.fn.visible = function(partial) {
+              
+          //       var $t            = $(this),
+          //           $w            = $(window),
+          //           viewTop       = $w.scrollTop(),
+          //           viewBottom    = viewTop + $w.height(),
+          //           _top          = $t.offset().top,
+          //           _bottom       = _top + $t.height(),
+          //           compareTop    = partial === true ? _bottom : _top,
+          //           compareBottom = partial === true ? _top : _bottom;
+              
+          //     return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+          //   };
+              
+          // })(jQuery);
+
+          // var win = $(window);
+
+          // var allMods = $(".grid-item");
+
+          // allMods.each(function(i, el) {
+          //   var el = $(el);
+          //   if (el.visible(true)) {
+          //     el.addClass("already-visible"); 
+          //   } 
+          // });
+
+          // win.scroll(function(event) {
+            
+          //   allMods.each(function(i, el) {
+          //     var el = $(el);
+          //     if (el.visible(true)) {
+          //       el.addClass("come-in"); 
+          //     } 
+          //   });
+            
+          // });
+
+
             
         });
     })
